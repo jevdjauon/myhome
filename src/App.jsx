299866c34eec6f1956783662d414e7e6b03ea2b4
jevdjauon <a.jevdjenijevic24@gmail.com>
2023-06-17 13:02:27 +0200

    Update starting files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-function App() {
+const App = () => {
   return (
-    <>
-      <h1>Helow</h1>
-    </>
+    <div>
+      <h1>MyHome</h1>
+      <div>
+        <h1>navbar</h1>
+        <p>weather</p>
+        <p>user bar</p>
+        <p>setting</p>
+      </div>
+      <div>searhbar?</div>
+      <div>favorite sites</div>
+      <div>
+        <h1>main</h1>
+        <div>main navbar</div>
+        <div>main dynamic content</div>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
