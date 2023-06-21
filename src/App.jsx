a765063144c50dbfa44d5d8de67d94a1685459ef
jevdjauon <a.jevdjenijevic24@gmail.com>
2023-06-21 23:54:15 +0200

    Add helpers, Add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
-import "./styles/app.scss";
+// Apps
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Components
 import BackgroundImage from "./components/BackgroundImage";
 import WeatherToolbar from "./components/WeatherWidget";
 import TimeWidget from "./components/TimeWidget";
+import Interests from "./components/Interests";
+
+// Other
+import "./styles/app.scss";
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <div className="app-container">
-      <BackgroundImage />
-      <header>
-        <TimeWidget />
-        <WeatherToolbar />
-        <p>user bar</p>
-        <p>setting</p>
-      </header>
-      <div>searhbar?</div>
-      <div>favorite sites</div>
-      <div>
-        <h1>main</h1>
-        <div>main navbar</div>
-        <div>main dynamic content</div>
+    <QueryClientProvider client={queryClient}>
+      <div className="app-container">
+        <BackgroundImage />
+        <header>
+          <TimeWidget />
+          <WeatherToolbar />
+          <p>user bar</p>
+          <p>setting</p>
+        </header>
+        <div>searhbar?</div>
+        <div>favorite sites</div>
+        <Interests />
       </div>
-    </div>
+    </QueryClientProvider>
   );
 };
 
---
+++ src/helpers/interestsFetchData.js
@@ -0,0 +1,17 @@
+export const interestsFetchData = async () => {
+  const apiOptions = {
+    method: "GET",
+    //   url: "https://crypto-news16.p.rapidapi.com/news/top/5",
+    headers: {
+      "X-RapidAPI-Key": "a3dd6d81e5msh9406048f565d5b3p1cbc96jsn35ac2878db39",
+      "X-RapidAPI-Host": "crypto-news16.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await axios.request(apiOptions);
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
