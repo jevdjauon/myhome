b2ae28c3b2fc59c2c16a618581e29add6551fbe0
jevdjauon <a.jevdjenijevic24@gmail.com>
2023-06-28 22:07:06 +0200

    Add BackgroundImage.jsx

--- src/components/BackgroundImage.jsx
+++ src/components/BackgroundImage.jsx
@@ -1,16 +1,32 @@
+import axios from "axios";
+import { Link } from "react-router-dom";
 import "../styles/backgroundImage.scss";
 
-const imgUrl = "https://source.unsplash.com/featured/1920x1080";
+import { imageFetchData } from "../helpers/imageFetchData";
+import { useQuery } from "@tanstack/react-query";
 
 const BackgroundImage = () => {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["background-image"],
+    queryFn: imageFetchData,
+    staleTime: 60 * (60 * 1000),
+    cacheTime: 120 * (60 * 1000),
+  });
+
+  if (isLoading) return "Loading...";
+  if (error) return error.message;
+
+  let bgImageUrl = data.photos[0].src.landscape;
+
   return (
     <div className="bg-container">
       <div
         className="bg-image"
         style={{
-          backgroundImage: `url(${imgUrl})`,
+          backgroundImage: `url(${bgImageUrl})`,
         }}
       ></div>
+      <Link to="https://www.pexels.com">Photos provided by Pexels</Link>
     </div>
   );
 };
