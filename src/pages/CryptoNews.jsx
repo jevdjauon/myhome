78a87700831467a67509eaa113bdf3a42d831abd
jevdjauon <a.jevdjenijevic24@gmail.com>
2023-07-09 23:36:59 +0200

    Update CryptoNews.jsx

--- src/pages/CryptoNews.jsx
+++ src/pages/CryptoNews.jsx
@@ -7,17 +7,12 @@ import testData from "../data/interestsTestData.json";
 import { cryptoFetchData } from "../helpers/cryptoFetchData.js";
 
 const CryptoNews = () => {
-  // /*
-  // comment out before build
-
   const { isLoading, error, data } = useQuery({
     queryKey: ["interests-data"],
     queryFn: cryptoFetchData,
   });
   if (isLoading) return "Loading...";
   if (error) return "An error has occurred: " + error.message;
-  // console.log(data);
-  // */
 
   return (
     <div>
