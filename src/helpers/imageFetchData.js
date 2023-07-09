3a15d567803274a5feaa34d4de5f1406837bff4c
jevdjauon <a.jevdjenijevic24@gmail.com>
2023-07-09 23:36:28 +0200

    Update imageFetchData.js

--- src/helpers/imageFetchData.js
+++ src/helpers/imageFetchData.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const imageFetchData = async () => {
+export const imageFetchData = async ({ quary = "mountains" }) => {
   const options = {
     method: "GET",
-    url: "https://api.pexels.com/v1/search?query=nature&per_page=1",
+    url: `https://api.pexels.com/v1/search?query=${quary}&per_page=10`,
     headers: {
       Authorization: "rhoMW214PqLV090Bw9aR2kIcDLmULyTFRVNWfkE6EIk9HmMS4Zf6Av8G",
     },
