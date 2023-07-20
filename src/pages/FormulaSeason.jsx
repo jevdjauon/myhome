3a6342dba6ee6ac7a5f185f41e723ed864bcb2a9
jevdjauon <a.jevdjenijevic24@gmail.com>
2023-07-20 15:22:54 +0200

    Update FormulaSeason.jsx

--- src/pages/FormulaSeason.jsx
+++ src/pages/FormulaSeason.jsx
@@ -1,24 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
 import formulaData from "../data/formulaTestData.json";
+import { formulaFetchData } from "../helpers/formulaFetchData";
 
 const FormulaSeason = () => {
   const nextRace = formulaData.nextRoundEvents;
 
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["formulaSeason"],
+    queryFn: formulaFetchData,
+    staleTime: 60 * (60 * 1000),
+    cacheTime: 60 * (60 * 1000),
+  });
+
   return (
     <div>
       <h3>Next Sesion</h3>
-      {nextRace.map(({ startDate, summary, location, status }) => (
-        <div key={startDate} style={{ margin: "20px" }}>
-          <div>
-            <p>{location}</p>
-            <p>{startDate}</p>
+      {data &&
+        data.nextRoundEvents.map(({ startDate, summary, location, status }) => (
+          <div key={startDate} style={{ margin: "20px" }}>
+            <div>
+              <p>{location}</p>
+              <p>{startDate}</p>
+            </div>
+            <h3>{summary}</h3>
+            <div>
+              <div className="status-icon"></div>
+              <p>{status}</p>
+            </div>
           </div>
-          <h3>{summary}</h3>
-          <div>
-            <div className="status-icon"></div>
-            <p>{status}</p>
-          </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
