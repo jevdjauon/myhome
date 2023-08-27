b7a9398dadfde54a081cb07066c26c422b949443
jevdjauon <a.jevdjenijevic24@gmail.com>
2023-08-27 12:16:40 +0200

    Update helpers

--- src/helpers/imageFetchData.js
+++ src/helpers/imageFetchData.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const imageFetchData = async ({ quary = "mountains" }) => {
+export const imageFetchData = async (props) => {
   const options = {
     method: "GET",
-    url: `https://api.pexels.com/v1/search?query=${quary}&per_page=10`,
+    url: `https://api.pexels.com/v1/search?query=${props}&per_page=10`,
     headers: {
       Authorization: "rhoMW214PqLV090Bw9aR2kIcDLmULyTFRVNWfkE6EIk9HmMS4Zf6Av8G",
     },
--- src/helpers/newsFetchData.js
+++ src/helpers/newsFetchData.js
@@ -2,17 +2,16 @@ import axios from "axios";
 
 export const newsFetchData = async () => {
   const options = {
-    method: "GET",
-    url: "https://newsdata2.p.rapidapi.com/news",
+    method: "POST",
+    url: "https://newsnow.p.rapidapi.com/",
     headers: {
+      "content-type": "application/json",
       "X-RapidAPI-Key": "a3dd6d81e5msh9406048f565d5b3p1cbc96jsn35ac2878db39",
-      "X-RapidAPI-Host": "newsdata2.p.rapidapi.com",
+      "X-RapidAPI-Host": "newsnow.p.rapidapi.com",
     },
-    params: {
-      country: "us, gb, fr",
-      //   category: "sports, health, entertainment",
-      language: "en",
-      q: "title, image",
+    data: {
+      text: "Top news",
+      region: "eu",
     },
   };
 
